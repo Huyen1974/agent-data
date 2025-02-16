b155dca814b5517c363f5d0d2f7c4df15e780de9
Nguyễn Minh Huyên <nmhuyen@gmail.com>
2025-02-16 15:51:52 +0700

    Add Cloud Function code (function_1)

---
+++ functions/function_1/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    return jsonify({"message": "Hello from a deployed Cloud Function!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
