368789cdcf42594df5737fac4e2ec236f90ceda9
Nguyễn Minh Huyên <nmhuyen@gmail.com>
2025-02-17 17:24:42 +0700

    Ensure all files are up-to-date and correct

--- containers/container-test/main.py
+++ containers/container-test/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,4 @@ def hello_world():
     return 'Hello from Flask in a container!'
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
